31e549f3c12a65f3bd02f487717d871689a19c21
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-10 10:58:00 -0300

    primeros estilos form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useUser } from 'reactfire';
 import './App.css';
-import Login from './component/login/Login';
+//import Login from './component/login/Login';
 import Home from './component/home/Home';
+import Form from './component/form/form';
 
 function App() {
   const user = useUser();
 
   return (
     <div className="App col-12">
-      { !user && <Login /> }
+      { !user && <Form /> }
       { user && <Home /> }
     </div>
   );
