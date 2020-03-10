f165136caf0573b4d39f1f24a85b87c7683a47c2
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-10 16:22:03 -0300

    ajustando enrutado de registro/inicio de sesión

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { useUser } from 'reactfire';
+// import { useUser } from 'reactfire';
 import './App.css';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Login from './component/login/Login';
-import Home from './component/home/Home';
+import Register from './component/login/Register';
+import LoginScreen from './component/login/LoginScreen';
 
 function App() {
-  const user = useUser();
+  // const user = useUser();
 
   return (
+    <div>
+      <BrowserRouter>
+        <>
+          <LoginScreen />
+          <Route exact path="/" component={LoginScreen} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </>
+      </BrowserRouter>
+    </div>
+
+  /*
     <div className="App col-12">
       { !user && <Login /> }
       { user && <Home /> }
     </div>
+    */
   );
 }
 
