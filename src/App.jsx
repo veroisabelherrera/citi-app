3d10de922d5f619b75dc55d7d01ed0ed193d5d27
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-11 21:43:40 -0300

    ajustando detalles de pantallas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useUser } from 'reactfire';
+import { useUser, useFirebaseApp } from 'reactfire';
 import './App.css';
+import { Link } from 'react-router-dom';
 import LoginScreen from './component/login/LoginScreen';
-import Home from './component/home/Home';
-import { Route, Switch, BrowserRouter, Link } from 'react-router-dom';
-import Profile from './component/profile/profile';
-import Transfer from './component/Transfer/transfer';
 import Routing from './component/Routing';
 import 'firebase/auth';
-import { useFirebaseApp } from 'reactfire';
+
 import { Button } from './component/elements/Button';
 
 
@@ -23,17 +20,16 @@ function App() {
 
   return (
     <section className="App">
-        {!user && <LoginScreen />}
+      {!user && <LoginScreen />}
 
-
-
-<Routing />
-{user &&
-<div>
-<Link to="/login"><Button>Volver al home</Button></Link>
-  <button type="button" className="Btn-two" onClick={logout}>Cerrar sesión</button>
-</div>
-}
+      <Routing />
+      {user
+        && (
+        <div className="nav-buttons">
+          <Link to="/login"><button className="Btn-back">Volver al home</button></Link>
+          <button type="button" className="Btn-logut" onClick={logout}>Cerrar sesión</button>
+        </div>
+        )}
 
     </section>
   );
