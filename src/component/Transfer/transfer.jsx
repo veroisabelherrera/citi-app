10e5894b90ac94b3339eb703e1c0f467c9b59817
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-12 00:05:49 -0300

    push para pull

--- src/component/Transfer/transfer.jsx
+++ src/component/Transfer/transfer.jsx
@@ -6,8 +6,8 @@ import './Transfer.css';
 function Transfer() {
   return (
     <section className="container-btn-home">
-      
-      <a href="https://api.whatsapp.com/send?&text=https:https://datos-bancarios-e6ed7.web.app/" data-action="share/whatsapp/share" target="_blank"><button type="button" className="btn-home">Compartir Formulario</button></a>
+
+      <a href="https://api.whatsapp.com/send?&text=https:https://datos-bancarios-e6ed7.web.app/" data-action="share/whatsapp/share"><button type="button" className="btn-home">Compartir Formulario</button></a>
       <Link to="/contacts"><button type="button" className="btn-home">Mis contactos</button></Link>
 
     </section>
