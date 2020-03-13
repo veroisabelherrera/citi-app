dd47f1bba631171d33af57ded1e89811778e8f45
irenemonzon <irene.monzonm@gmail.com>
2020-03-12 22:10:24 -0300

    mas informacion de contacto y monto a transferir

--- src/component/contact/InfoContact.jsx
+++ src/component/contact/InfoContact.jsx
@@ -1,35 +1,28 @@
-//import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 
-const infoContact = (Contact) => {
+const InfoContact = (items) => {
 
-    console.log(Contact);
-    console.log(Contact.email);
-    console.log(Contact.name);
-    console.log(Contact.rut);
-    console.log(Contact.bank);
-    console.log(Contact.typeAccount);
-    console.log(Contact.numberAccount);
-
-    /*  const [name, setName] = useState([]);
-      const [rut, setRut] = useState();
-      const [email, setEmail] = useState();
-      const [bank, setBank] = useState();
-      const [typeAccount, setTypeAccount] = useState();
-      const [numberAccount, setNumberAccount] = useState('');
-  
-    
-  
-      return (
-          <div>
-                      <h3>{name}</h3>
-                      <p>{item.bank}</p>
-                      <p>{item.email}</p>
-                      <p>{item.rut}</p>
-                      <p>{item.typeAccount}</p>
-                      <p>{item.numberAccount}</p>
-                  
-          </div>
-      )*/
+    const { name, bank, email, rut, typeAccount, numberAccount } = items.location.state.propPassed
+    const [amount, setAmount] = useState('');
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{bank}</p>
+            <p>{email}</p>
+            <p>{rut}</p>
+            <p>{typeAccount}</p>
+            <p>{numberAccount}</p>
+            <br />
+            <label className="H3">Monto :</label>
+            <input
+                className="input"
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.currentTarget.value)}
+            />
+            <button>Transferir</button>
+        </div>
+    )
 }
-export default infoContact;
+export default InfoContact;
