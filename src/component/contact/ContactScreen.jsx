78e6d7818911758430d1d34ea5836c29ad2b7db6
Veronica Isabel <veronica@Veronica-Isabel.local>
2020-03-12 23:01:05 -0300

    cambiando colores a negro

--- src/component/contact/ContactScreen.jsx
+++ src/component/contact/ContactScreen.jsx
@@ -9,7 +9,7 @@ export default () => (
       <div className="container-new-contact">
       <H2>Nuevo contacto</H2>
       <a href="https://api.whatsapp.com/send?&text=https:https://datos-bancarios-e6ed7.web.app/" data-action="share/whatsapp/share" target="_blank"><button type="button" className="btn-new-contact">Pedir datos a contacto de WhatsApp</button></a>
-      <Link to="/form"><button type="button" className="btn-new-contact-2">Ingresar datos de nuevo contacto</button></Link>
+      <Link to="/form"><button type="button" className="btn-new-contact">Ingresar datos de nuevo contacto</button></Link>
       </div>
 
       <H2>Contactos guardados</H2>
